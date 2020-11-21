ea56d7a2fba7d85d240435800ba62c9d31f31340
Fariz Izwan Kamaruzzaman <malikperang@gmail.com>
2020-11-22 01:16:44 +0800

    update

--- up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd elasticsearch-docker && docker-compose up --build -d
-cd ..
-cd cortex-vagrant && vagrant up
-cd ..
-cd misp-vagrant && vagrant up
-cd ..
-cd thehive-vagrant && vagrant up
